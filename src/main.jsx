4accb7d832aed19dbb69d954326443d275a601f1
ikennaex <ikennaexcel@gmail.com>
2025-11-02 12:13:10 +0100

    update: import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { CartProvider } from "./ecommerce/contexts/CartContext.jsx";
+import { CartProvider } from "./ecommerce/Contexts/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
