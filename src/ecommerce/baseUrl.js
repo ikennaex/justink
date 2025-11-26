e3d8f5150da7828a6bb3f9dba30a67e870340146
ikennaex <ikennaexcel@gmail.com>
2025-11-26 11:43:25 +0100

    Update: devMode

--- src/ecommerce/baseUrl.js
+++ src/ecommerce/baseUrl.js
@@ -1,6 +1,6 @@
 const isDev = false; // set to true for local development
 const localhost = "http://localhost:4000/"
-const live = "import.meta.env.VITE_LIVE_HOST"
+const live = import.meta.env.VITE_LIVE_HOST
 
 export const baseUrl = isDev
   ? localhost
